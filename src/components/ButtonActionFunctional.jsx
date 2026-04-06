321261439da78f24740c5285dbda6d1c7ea6df85
Priyanshi Mittal <priyanshimzn05@gmail.com>
2026-04-06 11:08:18 +0530

    button-action-1

---
+++ src/components/ButtonActionFunctional.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+const ButtonActionFunctional=()=>{
+  const [buttonAction,setButtonAction]=useState("")
+  function handlePara()
+  {
+    setButtonAction("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.")
+  }
+  return (
+    <div>
+      <button id="click" onClick={handlePara}>Click</button>
+      <p id="para">{buttonAction}</p>
+    </div>
+  )
+}
+
+export default ButtonActionFunctional
